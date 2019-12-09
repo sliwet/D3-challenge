7147e3ad9f46b3e9f5675f686db1434a903ccee2
Daewon Kwon <sliwet@gmail.com>
2019-12-08 22:58:10 -0500

    Reviewing previous example

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -1,30 +1,32 @@
-var svgWidth = 960;
-var svgHeight = 500;
-var margin = { top: 20, right: 40, bottom: 80, left: 100 };
-var width = svgWidth - margin.left - margin.right;
-var height = svgHeight - margin.top - margin.bottom;
+let svgWidth = 960;
+let svgHeight = 500;
+let margin = { top: 20, right: 40, bottom: 80, left: 100 };
+let width = svgWidth - margin.left - margin.right;
+let height = svgHeight - margin.top - margin.bottom;
 
-var svg = d3
-  .select("#scatter")
-  .append("svg")
-  .attr("width", svgWidth)
-  .attr("height", svgHeight);
+let svg = d3.select("#scatter").append("svg").attr("width", svgWidth).attr("height", svgHeight);
+let chartGroup = svg.append("g").attr("transform", `translate(${margin.left}, ${margin.top})`);
 
-var chartGroup = svg.append("g")
-  .attr("transform", `translate(${margin.left}, ${margin.top})`);
-
-var chosenXAxis = "hair_length";
+// Functions
 
 let xScale = (hairData, chosenXAxis) => {
-  var xLinearScale = d3.scaleLinear()
+  let xLinearScale = d3.scaleLinear()
     .domain([d3.min(hairData, d => d[chosenXAxis]) * 0.8, d3.max(hairData, d => d[chosenXAxis]) * 1.2])
     .range([0, width]);
 
   return xLinearScale;
 }
 
+let yScale = (hairData, chosenYAxis) => {
+  let yLinearScale = d3.scaleLinear()
+    .domain([0, d3.max(hairData, d => d[chosenYAxis])])
+    .range([height, 0]);
+
+  return yLinearScale;
+}
+
 let renderAxes = (newXScale, xAxis) => {
-  var bottomAxis = d3.axisBottom(newXScale);
+  let bottomAxis = d3.axisBottom(newXScale);
   xAxis.transition()
     .duration(1000)
     .call(bottomAxis);
@@ -39,14 +41,15 @@ let renderCircles = (circlesGroup, newXScale, chosenXaxis) => {
 }
 
 let updateToolTip = (chosenXAxis, circlesGroup) => {
+  let label = "";
   if (chosenXAxis === "hair_length") {
-    var label = "Hair Length:";
+    label = "Hair Length:";
   }
   else {
-    var label = "# of Albums:";
+    label = "# of Albums:";
   }
 
-  var toolTip = d3.tip()
+  let toolTip = d3.tip()
     .attr("class", "tooltip")
     .offset([80, -60])
     .html(d => `${d.rockband}<br>${label} ${d[chosenXAxis]}`);
@@ -60,72 +63,96 @@ let updateToolTip = (chosenXAxis, circlesGroup) => {
   return circlesGroup;
 }
 
+let chosenXAxis = "hair_length";
+let chosenYAxis = "num_hits";
+
 d3.csv("hairdata.csv").then((hairData, err) => {
   if (err) throw err;
 
-  hairData.forEach(function (data) {
+  // Data =========================
+
+  hairData.forEach(data => {
     data.hair_length = +data.hair_length;
     data.num_hits = +data.num_hits;
     data.num_albums = +data.num_albums;
   });
 
-  var xLinearScale = xScale(hairData, chosenXAxis);
+  // X and Y axis =================
 
-  var yLinearScale = d3.scaleLinear()
-    .domain([0, d3.max(hairData, d => d.num_hits)])
-    .range([height, 0]);
+  let xLinearScale = xScale(hairData, chosenXAxis);
+  let yLinearScale = yScale(hairData, chosenYAxis);
 
-  var bottomAxis = d3.axisBottom(xLinearScale);
-  var leftAxis = d3.axisLeft(yLinearScale);
+  let bottomAxis = d3.axisBottom(xLinearScale);
+  let leftAxis = d3.axisLeft(yLinearScale);
 
-  var xAxis = chartGroup.append("g")
+  let xAxis = chartGroup.append("g")
     .classed("x-axis", true)
     .attr("transform", `translate(0, ${height})`)
     .call(bottomAxis);
 
-  chartGroup.append("g")
+  let yAxis = chartGroup.append("g")
+    .classed("y-axis", true)
     .call(leftAxis);
 
-  var circlesGroup = chartGroup.selectAll("circle")
-    .data(hairData)
-    .enter()
-    .append("circle")
-    .attr("cx", d => xLinearScale(d[chosenXAxis]))
-    .attr("cy", d => yLinearScale(d.num_hits))
-    .attr("r", 20)
-    .attr("fill", "pink")
-    .attr("opacity", ".5");
+  // Labels for X and Y axis  =================
 
-  var labelsGroup = chartGroup.append("g")
+  let xLabelsGroup = chartGroup.append("g")
     .attr("transform", `translate(${width / 2}, ${height + 20})`);
 
-  var hairLengthLabel = labelsGroup.append("text")
+  let hairLengthLabel = xLabelsGroup.append("text")
     .attr("x", 0)
     .attr("y", 20)
     .attr("value", "hair_length") // value to grab for event listener
     .classed("active", true)
     .text("Hair Metal Ban Hair Length (inches)");
 
-  var albumsLabel = labelsGroup.append("text")
+  let albumsLabel = xLabelsGroup.append("text")
     .attr("x", 0)
     .attr("y", 40)
     .attr("value", "num_albums") // value to grab for event listener
     .classed("inactive", true)
     .text("# of Albums Released");
 
-  chartGroup.append("text")
+  let yLabelsGroup = chartGroup.append("g")
     .attr("transform", "rotate(-90)")
-    .attr("y", 0 - margin.left)
-    .attr("x", 0 - (height / 2))
+
+  let oneYLabel = yLabelsGroup.append("text")
+    .attr("y", -margin.left)
     .attr("dy", "1em")
+    .attr("x", -height / 2)
     .classed("axis-text", true)
+    // .attr("value", "hair_length") // value to grab for event listener
+    .classed("axis-text", true)
+    .classed("active", true)
     .text("Number of Billboard 500 Hits");
 
-  var circlesGroup = updateToolTip(chosenXAxis, circlesGroup);
+  let anotherYLabel = yLabelsGroup.append("text")
+    .attr("y", -margin.left / 2)
+    .attr("dy", "1em")
+    .attr("x", -height / 2)
+    .classed("axis-text", true)
+    // .attr("value", "hair_length") // value to grab for event listener
+    .classed("axis-text", true)
+    .classed("inactive", true)
+    .text("Number of Billboard 500 Hits");
 
-  labelsGroup.selectAll("text")
+  // Plotting data  =================
+
+  let circlesGroup = chartGroup.selectAll("circle")
+    .data(hairData)
+    .enter()
+    .append("circle")
+    .attr("cx", d => xLinearScale(d[chosenXAxis]))
+    .attr("cy", d => yLinearScale(d[chosenYAxis]))
+    .attr("r", 20)
+    .attr("fill", "pink")
+    .attr("opacity", ".5");
+
+  circlesGroup = updateToolTip(chosenXAxis, circlesGroup);
+
+  xLabelsGroup.selectAll("text")
     .on("click", () => {
-      var value = d3.select(d3.event.target).attr("value");
+      let value = d3.select(d3.event.target).attr("value");
       if (value !== chosenXAxis) {
         // replaces chosenXAxis with value
         chosenXAxis = value;
@@ -157,6 +184,6 @@ d3.csv("hairdata.csv").then((hairData, err) => {
         }
       }
     });
-}).catch(function (error) {
+}).catch(error => {
   console.log(error);
 });
